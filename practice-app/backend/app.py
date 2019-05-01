26a5561933c1c1e2d7fc6626cc5ea5bec93995ae
Alper Çakan <alpercakan98@gmail.com>
2019-05-02 00:25:59 +0300

    add the first endpoint: check API health

--- practice-app/backend/app.py
+++ practice-app/backend/app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/health', methods=['GET'])
+def get_health():
+    return jsonify({'status': 'OK'})
