3f6d5ccb8a401c3cadda99dab6b845baf3474ebf
Sercan <ersoysercan57@gmail.com>
2019-05-04 18:46:46 +0300

    updated comment model for db

--- practice-app/backend/app.py
+++ practice-app/backend/app.py
@@ -25,15 +25,19 @@ BaseModel.set_session(session)
 
 class Comment(BaseModel):
     __tablename__ = 'comment'
-    __repr_attrs__ = ['id', 'author', 'content']
     id = sa.Column(sa.Integer, primary_key=True)
     author = sa.Column(sa.String)
     content = sa.Column(sa.String)
+	tr_eq_sym = sa.Column(sa.String)
+	created_at = sa.Column(sa.DateTime)
 
     def serialize(self):
         return {
             'author': self.author,
-            'content': self.content
+            'content': self.content,
+			'id': self.id,
+			'tr_eq_sym': self.tr_eq_sym,
+			'created_at': self.created_at
         }
 
 ### Routes
