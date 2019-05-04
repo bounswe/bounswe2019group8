8a26e1f0c8c8bb559e077db5f24a571121063de6
Alper Çakan <alpercakan98@gmail.com>
2019-05-05 01:03:27 +0300

    create the base code for the trading equipment "profile" page

--- practice-app/frontend/mercatus/src/App.js
+++ practice-app/frontend/mercatus/src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
+import TradingEquipment from './TradingEquipment';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://127.0.0.1:5000';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+            <nav>
+                <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about/">About</Link>
+                </li>
+                <li>
+                    <Link to="/users/">Users</Link>
+                </li>
+                </ul>
+            </nav>
+
+            <Route path="/tr-eqs/:symbol" component={TradingEquipment} />
+            </div>
+        </Router>
+        );
 }
 
 export default App;
