5578acea9969403f70a55202e0cc7de9dfc089d8
Alper Çakan <alpercakan98@gmail.com>
2019-05-05 10:03:30 +0300

    implement showing the comments left about a trading equipment

--- practice-app/frontend/mercatus/src/App.js
+++ practice-app/frontend/mercatus/src/App.js
@@ -5,6 +5,7 @@ import TradingEquipment from './TradingEquipment';
 import axios from 'axios';
 
 axios.defaults.baseURL = 'http://127.0.0.1:5000';
+axios.defaults.headers.post['Content-Type'] = 'text/json';
 
 function App() {
     return (
