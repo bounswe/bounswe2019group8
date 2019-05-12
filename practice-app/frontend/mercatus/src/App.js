f52b97bf1858ce4a8711e68df113c0e1904becb1
Alper Çakan <alpercakan98@gmail.com>
2019-05-12 13:25:51 +0300

    update the home page and the routes

--- practice-app/frontend/mercatus/src/App.js
+++ practice-app/frontend/mercatus/src/App.js
@@ -1,19 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import './App.css';
+import TradingEquipment from './components/TradingEquipment';
+import EqList from './components/EqList';
+import axios from 'axios';
 
-import Toolbar from './components/Toolbar/Toolbar';
-import Table from './components/Table/Table';
+axios.defaults.baseURL = 'http://127.0.0.1:5000';
+axios.defaults.headers.post['Content-Type'] = 'text/json';
 
+function App() {
+    return (
+        <Router>
+            <div>
+            <nav className="navigation-bar">
+                Navigation:
+                <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/list/">List of Equipments</Link>
+                </li>
+                </ul>
+            </nav>
 
-class App extends Component {
-  render()
-    {
-        return (
-            <div className="App">
-                <Toolbar/>
-                <Table/>
+            <Route path="/tr-eqs/:symbol" component={TradingEquipment} />
+            <Route path="/list" component={EqList} />
             </div>
+        </Router>
         );
-    }
 }
 
 export default App;
