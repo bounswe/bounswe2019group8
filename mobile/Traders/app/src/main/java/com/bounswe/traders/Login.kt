341b3a7092c24a23c442d524f462f9570957fdbe
Volkan Yılmaz <volkan.yilmazboun@gmail.com>
2019-10-07 21:22:03 +0300

    Initialize the mobile

---
+++ mobile/Traders/app/src/main/java/com/bounswe/traders/Login.kt
@@ -0,0 +1,22 @@
+package com.bounswe.traders
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_login.*
+
+class Login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
