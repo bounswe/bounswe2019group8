a070779fefb7a596b40e7cf477026722ec6e9c00
Alper Çakan <alpercakan98@gmail.com>
2019-10-18 00:24:56 +0300

    implement authentication, user profile update and user listing

---
+++ nova/nova/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('users/', views.users_coll),
+    path('users/<int:pk>', views.user_res),
+
+    path('auth_tokens/', views.auth_tokens_coll)
+]
