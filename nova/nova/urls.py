e10b27d4e91a4ea8ccf02275dd831224cde1da92
Sercan Ersoy <sercanersoy5@gmail.com>
2019-10-19 14:16:04 +0300

    delete trailing slashes from urls

--- nova/nova/urls.py
+++ nova/nova/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/', views.users_coll),
+    path('users', views.users_coll),
     path('users/<int:pk>', views.user_res),
 
-    path('auth_tokens/', views.auth_tokens_coll)
+    path('auth_tokens', views.auth_tokens_coll)
 ]
