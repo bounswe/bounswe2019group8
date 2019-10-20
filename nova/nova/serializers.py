6ef1c628352350f8caf43afd4757b6a393847986
Sercan Ersoy <sercanersoy5@gmail.com>
2019-10-20 19:27:21 +0300

    check if password is sent to PUT request

--- nova/nova/serializers.py
+++ nova/nova/serializers.py
@@ -29,3 +29,8 @@ class UserSerializer(NovaSerializer):
         validated_data['password'] = make_password(validated_data.get('password'))
 
         return super(UserSerializer, self).create(validated_data)
+
+    def update(self, instance, validated_data):
+        if 'password' in validated_data:
+            validated_data['password'] = make_password(validated_data['password'])
+        return super(UserSerializer, self).update(instance, validated_data)
