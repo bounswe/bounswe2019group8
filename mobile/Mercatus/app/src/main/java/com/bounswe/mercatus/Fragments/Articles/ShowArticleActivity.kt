3745ebf5309ce8007c47239094a7a338655d2f55
Volkan Yılmaz <volkan.yilmazboun@gmail.com>
2019-11-20 17:11:02 +0300

    Show comments for articles is implemented

--- mobile/Mercatus/app/src/main/java/com/bounswe/mercatus/Fragments/Articles/ShowArticleActivity.kt
+++ mobile/Mercatus/app/src/main/java/com/bounswe/mercatus/Fragments/Articles/ShowArticleActivity.kt
@@ -6,9 +6,14 @@ import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bounswe.mercatus.API.ApiInterface
 import com.bounswe.mercatus.API.RetrofitInstance
+import com.bounswe.mercatus.Adapters.CommentAdapter
 import com.bounswe.mercatus.Fragments.ShowProfileActivity
+import com.bounswe.mercatus.Models.CommentBody
+import com.bounswe.mercatus.Models.CommentShowBody
 import com.bounswe.mercatus.Models.GetArticleBody
 import com.bounswe.mercatus.Models.UserRes
 import com.bounswe.mercatus.R
@@ -35,6 +40,18 @@ class ShowArticleActivity : AppCompatActivity() {
         val articleID = intent.getStringExtra("article_header")
 
         getArticle(title, content, author, articleID!!.toInt())
+
+        val rv = findViewById<RecyclerView>(R.id.recyclerViewComments)
+        rv.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+
+        val commentsList = ArrayList<CommentShowBody>()
+
+        commentsList.add(CommentShowBody(15,"sss", 1,1))
+        commentsList.add(CommentShowBody(15,"sss", 1,1))
+        var adapter = CommentAdapter(this@ShowArticleActivity, commentsList)
+        rv.adapter = adapter
+
+        getComments(commentsList, adapter)
 
         fab.setOnClickListener {
             val intent = Intent(this@ShowArticleActivity, CreateCommentActivity::class.java)
@@ -119,6 +136,47 @@ class ShowArticleActivity : AppCompatActivity() {
                 }
                 else  {
                     Toast.makeText(this@ShowArticleActivity, "Show profile failed.", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+        })
+    }
+
+    private fun getComments(commentsList: ArrayList<CommentShowBody>, adapter: CommentAdapter){
+        val mer = RetrofitInstance.getRetrofitInstance().create(ApiInterface::class.java)
+
+        val res = getSharedPreferences("TOKEN_INFO", Context.MODE_PRIVATE)
+        val tokenV = res?.getString("token", "Data Not Found!")
+
+
+        mer.getComments(5,"Token " + tokenV.toString()).enqueue(object :
+            Callback<List<CommentBody>> {
+            override fun onFailure(call: Call<List<CommentBody>>, t: Throwable) {
+                if(t.cause is ConnectException){
+                    Toast.makeText(
+                        this@ShowArticleActivity,
+                        "Check your connection!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                else{
+                    Toast.makeText(
+                        this@ShowArticleActivity,
+                        t.message,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+            override fun onResponse(call: Call<List<CommentBody>>, response: Response<List<CommentBody>>) {
+                if (response.code() == 200) {
+                    val res: List<CommentBody>? = response.body()
+                    for(i in res.orEmpty()){
+                        commentsList.add(CommentShowBody(i.author, i.content, i.article , i.pk))
+                    }
+                    adapter.notifyDataSetChanged()
+                }
+                else  {
+                    Toast.makeText(this@ShowArticleActivity, "Get articles failed.", Toast.LENGTH_SHORT)
                         .show()
                 }
             }
