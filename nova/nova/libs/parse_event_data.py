80e1b3f45568cd4688393b16f32745f29e3ca9fd
Alper Çakan <alpercakan98@gmail.com>
2019-11-23 17:04:59 +0300

    implement 3rd party events api parser

---
+++ nova/nova/libs/parse_event_data.py
@@ -0,0 +1,39 @@
+import re
+
+
+def clear_value(s):
+    s = s.strip()
+    if s == "&nbsp;":
+        return ""
+
+    return s
+
+
+def search_captured(pattern, s):
+    res = re.search(pattern, s)
+
+    if res is None:
+        return ""
+
+    return res.group(1)
+
+
+def parse_event_data(data, date):
+    events = []
+
+    for match in re.finditer(r"<tr\s*id=\"eventRowId_(.*?)\".*?>.*?</tr>", data):
+        row = match.group(0)
+
+        print(row)
+        events.append({
+            "id": match.group(1),
+            "country": search_captured(r"title=\"(.*?)\"\s+class=\"ceFlags", row),
+            "time": search_captured(r"js-time\"\s*>(.*?)</td>", row),
+            "value": clear_value(search_captured(r"id=\"eventActual_\d+\">(.*?)</td>", row)),
+            "predicted": clear_value(search_captured(r"id=\"eventForecast_\d+\">(.*?)</td>", row)),
+            "name": search_captured(r"data-name\s*=\"(.*?)\"", row),
+            "importance": search_captured(r"data-img_key=\"bull(\d)\"", row),
+            "date": date
+        })
+
+    return events
