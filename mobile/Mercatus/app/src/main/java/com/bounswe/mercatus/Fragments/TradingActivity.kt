72f5c7a5ca3b598f69bc58870f0398332a6b4bfe
Volkan Yılmaz <volkan.yilmazboun@gmail.com>
2019-11-23 17:30:49 +0300

    Charts are added to trading equipments

---
+++ mobile/Mercatus/app/src/main/java/com/bounswe/mercatus/Fragments/TradingActivity.kt
@@ -0,0 +1,18 @@
+package com.bounswe.mercatus.Fragments
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bounswe.mercatus.R
+
+class TradingActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trading)
+
+
+    }
+
+}
