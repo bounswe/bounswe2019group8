193c4828f64bd75e73fad121f562e09ff7771fae
Volkan Yılmaz <volkan.yilmazboun@gmail.com>
2019-11-24 16:21:17 +0300

    Add zoom in chart screen in landscape mode.

--- mobile/Mercatus/app/src/main/java/com/bounswe/mercatus/Models/PriceModel.kt
+++ mobile/Mercatus/app/src/main/java/com/bounswe/mercatus/Models/PriceModel.kt
@@ -1,4 +1,13 @@
 package com.bounswe.mercatus.Models
 
-class PriceModel {
-}
+
+// This body is designed to get each forex item parity value.
+data class PriceModel(
+    val interval_category: String,
+    val observed_at: String,
+    val tr_eq: String,
+    val open: String,
+    val close: String,
+    val high: String,
+    val low: String
+)
