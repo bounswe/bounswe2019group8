c49cb09a65fd990830355e2f090f12ccdb9acd06
Sercan Ersoy <sercanersoy5@gmail.com>
2019-12-12 16:00:51 +0300

    add daily method for fetching prices monthly

--- nova/nova/nasdaq.py
+++ nova/nova/nasdaq.py
@@ -1,4 +1,6 @@
 import requests
+from dateutil.relativedelta import relativedelta
+from django.db.models import Q
 from django.utils.datetime_safe import datetime, date
 from rest_framework import permissions
 from rest_framework.decorators import api_view, permission_classes
@@ -45,6 +47,69 @@ def fetch_commodities_intradaily():
             ask_value=ask_value,
             interval='intraday',
         )
+
+
+def fetch_commodities_daily():
+    commodities = TradingEquipment.objects.filter(type='commodity')
+
+    today = date.today()
+    last_month = today - relativedelta(months=1)
+
+    for commodity in commodities:
+        url = NASDAQ_BASE_URL + '/' + commodity.sym + '/historical'
+
+        response = requests.get(url, params={
+            'assetclass': 'commodities',
+            'limit': 30,
+            'fromdate': last_month,
+            'todate': today,
+        })
+
+        if response.status_code != 200:
+            print('Error occurred while fetching ', commodity.sym)
+            continue
+
+        json_response = response.json()
+
+        rows = json_response['data']['tradesTable']['rows']
+
+        for row in rows:
+            date_tokens = row['date'].split('/')
+            date_str = date_tokens[2] + '-' + date_tokens[0] + '-' + date_tokens[1]
+
+            price_dict = {
+                'open': row['open'],
+                'close': row['close'],
+                'high': row['high'],
+                'low': row['low'],
+            }
+
+            for key, value in price_dict.items():
+                Price.objects.create(
+                    observe_date=date_str,
+                    observe_time=None,
+                    tr_eq=commodity,
+                    indicative_value=value,
+                    bid_value=None,
+                    ask_value=None,
+                    interval=key,
+                )
+
+
+def delete_old_prices():
+    """
+    This method deletes intraday prices starting from yesterday (exclusive), and all price objects that are not intraday.
+    """
+
+    today = date.today()
+    yesterday = today - relativedelta(days=1)
+
+    prices_to_delete = Price.objects.filter(
+        ~Q(interval='intraday') |
+        (Q(interval='intraday') & Q(observe_date__lt=yesterday))
+    )
+
+    prices_to_delete.delete()
 
 
 @api_view(['POST'])
