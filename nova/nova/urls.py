f18c60c859df5b7d5791b83bb287a1a5e289312b
Sercan Ersoy <sercanersoy5@gmail.com>
2019-12-13 23:25:57 +0300

    resolve import problem that came from merge

--- nova/nova/urls.py
+++ nova/nova/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from nova.other_views import article_view, trading_eq_view, comment_view, like_view, dislike_view, \
-    events as events_views
+    events as events_views, portfolio_view
 from . import views, nasdaq
 from .swagger import get_swagger_view
 
