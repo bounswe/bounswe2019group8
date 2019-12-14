ed177b974f7f3561ecfe55b34a430fb783314692
efe-onal-2016400267 <32375462+efe-onal-2016400267@users.noreply.github.com>
2019-12-14 16:25:37 +0300

    Create Portfolio feature is ready, waiting for backend deploy.

---
+++ frontend/my-app/src/components/portfolio_components/createPortfolio.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import {
+  FormGroup,
+  FormControl,
+  FormLabel,
+  Button,
+  FormText,
+  Badge
+} from "react-bootstrap";
+import { useFormFields } from "../../libs/hooksLib";
+
+import axios from "axios";
+import { withRouter } from "react-router-dom";
+import LoaderButton from "../../containers/LoaderButton";
+
+export default function CreatePortfolio(submitClicked, ...props) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [switchOn, switchClicked] = useState(true);
+  const [fields, handleFieldChange] = useFormFields({
+    name: ""
+  });
+
+  function validateForm() {
+    return fields.name.length > 0;
+  }
+  function switchChange(){
+      switchClicked(!switchOn);
+      console.log(switchOn);
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    setIsLoading(true);
+    var token = localStorage.getItem("userToken");
+    console.log(token);
+    var switchVar = "False";
+    if(switchOn === true){
+        switchVar = "True";
+    }
+    else{
+        switchVar = "False";
+    }
+    var data = {
+      name: fields.name,
+      private: switchVar
+    };
+    
+    axios
+      .post("http://8.209.81.242:8000/portfolios", data, {
+        headers: { Authorization: `Token ${token}` }
+      })
+      .then(function (response) {
+        console.log(response);
+        window.location.reload();
+      });
+
+  }
+  return (
+    <div className="field-container">
+      <form onSubmit={handleSubmit}>
+        <FormGroup controlId="name" size="large">
+          <FormLabel>
+            <Badge>Portfolio Name</Badge>
+          </FormLabel>
+          <FormControl
+            autoFocus
+            type="name"
+            value={fields.name}
+            onChange={handleFieldChange}
+          />
+          <FormText className="text-muted">
+            This fields can be updated any time.
+            </FormText>
+        </FormGroup>
+
+        <div className='custom-control custom-switch'>
+        <input
+          onChange={switchChange}
+          type='checkbox'
+          className='custom-control-input'
+          id='customSwitches'
+          readOnly
+        />
+        <label className='custom-control-label' htmlFor='customSwitches'>
+          Set Private.
+        </label>
+      </div>
+
+        <LoaderButton
+          block
+          style={{ marginTop: 30, width: 200}}
+          type="submit"
+          size="sm"
+          variant='outline-primary'
+          isLoading={isLoading}
+          disabled={!validateForm()}
+        >
+          Create Portfolio
+          </LoaderButton>
+      </form>
+    </div>
+  );
+}
--- frontend/my-app/src/components/portfolio_components/ownPortfolioPage.jsx
+++ frontend/my-app/src/components/portfolio_components/ownPortfolioPage.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import {Button, Badge, Tabs, Tab} from "react-bootstrap";
 import "./ownPortfolioPage.css";
-import { FaFolderOpen} from "react-icons/fa";
+import CreatePortfolio from "./createPortfolio";
+import { FaFolderOpen, FaFolderPlus} from "react-icons/fa";
 
 class OwnPortfolioPage extends Component {
     state = {  }
@@ -11,11 +12,14 @@ class OwnPortfolioPage extends Component {
                 <Tabs defaultActiveKey="your-portfolios" style={{fontWeight:'lighter',  justifyContent: 'center', backgroundColor:'whitesmoke' , borderRadius:"3px"}}>
                     <Tab eventKey="your-portfolios" title={<div><FaFolderOpen></FaFolderOpen> Your Portfolios</div>}>
                         <div className="your-portfolios-div">
-                            <Button className="create-portfolio-button">Create a new portfolio</Button>
                         </div>
+                    </Tab>
+                    <Tab eventKey="create-portfolio" title={<div><FaFolderPlus></FaFolderPlus> Create a new Portfolio </div>} >
+                        <CreatePortfolio></CreatePortfolio>
                     </Tab>
                     <Tab eventKey="followed-portfolios" title={<div><FaFolderOpen></FaFolderOpen> Followed Portfolios </div>} >
                     </Tab>
+                    
                  </Tabs>
             </div>
          );
