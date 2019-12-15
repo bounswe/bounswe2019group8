dd17903975cee5ed6f9be753d9c1b666fb7c2234
Alper Çakan <alpercakan98@gmail.com>
2019-12-15 15:31:07 +0300

    fix the urls file according to new file structurte

--- nova/nova/urls.py
+++ nova/nova/urls.py
@@ -1,78 +1,88 @@
 from django.urls import path
 
-from nova.other_views import article_view, trading_eq_view, comment_view, like_view, dislike_view, \
-    events as events_views, portfolio_view
-from . import views, nasdaq
+from nova.views import articles as article_views, auth_tokens as auth_token_views, \
+    comments as  comment_views, events as event_views, likes as like_views, dislikes as dislike_views, \
+    orders as order_views, parities as parity_views, users as user_views, trading_equipments as trading_equipment_views, \
+    portfolios as portfolio_views
+from . import nasdaq
 from .swagger import get_swagger_view
 
 urlpatterns = [
-    path('', get_swagger_view(title='Mercatus API Documentation')),
+    # Swagger
+    path('', get_swagger_view(title='Mercatus - Nova API Documentation')),
 
-    path('users', views.users_coll),
+    # Users
+    path('users', user_views.users_coll),
 
-    path('users/<int:pk>', views.user_res),
+    path('users/<int:pk>', user_views.user_res),
 
-    path('users/<int:pk>/followings', views.user_followings_coll),
+    path('users/<int:pk>/followings', user_views.user_followings_coll),
 
-    path('users/<int:user_pk>/followings/<int:following_pk>', views.user_following_res),
+    path('users/<int:user_pk>/followings/<int:following_pk>', user_views.user_following_res),
 
-    path('users/<int:pk>/followers', views.user_followers_coll),
+    path('users/<int:pk>/followers', user_views.user_followers_coll),
 
-    path('users/<int:pk>/articles', article_view.article_of_users_res),
+    path('users/<int:pk>/articles', article_views.article_of_users_res),
 
-    path('auth_tokens', views.auth_tokens_coll),
+    path('user_searches', user_views.user_searches_res),
 
-    path('user_searches', views.user_searches_res),
+    # Auth tokens
+    path('auth_tokens', auth_token_views.auth_tokens_coll),
 
-    path('activations/<uidb64>/<token>', views.activate_account),
+    # Activation
+    path('activations/<uidb64>/<token>', user_views.activate_account),
 
-    path('articles', article_view.article_coll),
+    # Articles
+    path('articles', article_views.article_coll),
 
-    path('articles/<int:pk>', article_view.article_res),
+    path('articles/<int:pk>', article_views.article_res),
 
-    path('articles/<int:pk>/comments', comment_view.comment_coll_article),
+    path('articles/<int:pk>/comments', comment_views.comment_coll_article),
 
-    path('articles/<int:pk>/likes', like_view.likes_article_coll),
+    path('articles/<int:pk>/likes', like_views.likes_article_coll),
 
-    path('articles/<int:pk>/dislikes', dislike_view.dislikes_article_coll),
+    path('articles/<int:pk>/dislikes', dislike_views.dislikes_article_coll),
 
-    path('trading_equipments', trading_eq_view.trading_eq_coll),
+    # Trading Equipments
 
-    path('trading_equipments/digital', trading_eq_view.trading_eq_digital_coll),
+    path('trading_equipments', trading_equipment_views.trading_eq_coll),
 
-    path('trading_equipments/forex', trading_eq_view.trading_eq_forex_coll),
+    path('trading_equipments/digital', trading_equipment_views.trading_eq_digital_coll),
 
-    path('trading_equipments/<int:pk>', trading_eq_view.trading_eq_res),
+    path('trading_equipments/forex', trading_equipment_views.trading_eq_forex_coll),
 
-    path('trading_equipments/<int:pk>/comments', comment_view.comment_coll_tr_eq),
+    path('trading_equipments/<int:pk>', trading_equipment_views.trading_eq_res),
 
-    path('trading_equipments/<int:pk>/predictions/upvotes', trading_eq_view.upvotes_tr_eq),
+    path('trading_equipments/<int:pk>/comments', comment_views.comment_coll_tr_eq),
 
-    path('trading_equipments/<int:pk>/predictions/downvotes', trading_eq_view.downvotes_tr_eq),
+    path('trading_equipments/<int:pk>/predictions/upvotes', trading_equipment_views.upvotes_tr_eq),
 
-    path('comments/<int:pk>', comment_view.comment_res),
+    path('trading_equipments/<int:pk>/predictions/downvotes', trading_equipment_views.downvotes_tr_eq),
 
-    path('comments/<int:pk>/likes', like_view.likes_comment_coll),
+    path('trading_equipment_searches', trading_equipment_views.tr_eq_searches),
 
-    path('comments/<int:pk>/dislikes', dislike_view.dislikes_comment_coll),
+    # Comments
+    path('comments/<int:pk>', comment_views.comment_res),
 
-    path('trading_equipment_searches', trading_eq_view.tr_eq_searches),
-    
+    path('comments/<int:pk>/likes', like_views.likes_comment_coll),
+
+    path('comments/<int:pk>/dislikes', dislike_views.dislikes_comment_coll),
+
     # Portfolios
-    path('portfolios', portfolio_view.portfolio_ops),
+    path('portfolios', portfolio_views.portfolio_ops),
 
-    path('portfolios/<int:pk>', portfolio_view.portfolio_update),
+    path('portfolios/<int:pk>', portfolio_views.portfolio_update),
 
-    path('portfolios/<int:pk>/follows', portfolio_view.portfolio_follows),
+    path('portfolios/<int:pk>/follows', portfolio_views.portfolio_follows),
 
-    path('users/<int:pk>/portfolios', portfolio_view.portfolio_visitor),
+    path('users/<int:pk>/portfolios', portfolio_views.portfolio_visitor),
 
     # Events
-    path('events/<date>', events_views.events_coll),
+    path('events/<date>', event_views.events_coll),
 
     # TEMPORARY ENDPOINTS FOR TESTS
 
-    path('cnt', trading_eq_view.cnt),
+    path('cnt', trading_equipment_views.cnt),
 
     path('cron_jobs/nasdaq_intradaily', nasdaq.fetch_all_intradaily),
 
