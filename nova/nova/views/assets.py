8650c51d5921442d2631b0445bf72d8123d01a99
Alper Çakan <alpercakan98@gmail.com>
2019-12-15 22:40:38 +0300

    implement endpoint for adding cash to user account

---
+++ nova/nova/views/assets.py
@@ -0,0 +1,36 @@
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.exceptions import NotFound, PermissionDenied, ValidationError
+from rest_framework import permissions, status
+from rest_framework.response import Response
+
+from nova.models import User, TradingEquipment
+from nova.serializers import AssetSerializer
+from decimal import Decimal
+
+
+# users/<user_pk>/cash
+@api_view(['POST'])
+@permission_classes((permissions.IsAuthenticated,))
+def cash_coll(request, user_pk):
+    try:
+        user = User.objects.get(pk=user_pk)
+    except User.DoesNotExist:
+        raise NotFound()
+
+    if request.method == 'POST':
+        if request.user.pk != user_pk:
+            raise PermissionDenied()
+
+        try:
+            amount = Decimal(request.data['amount'])
+            if amount <= 0:
+                raise ValueError()
+        except:
+            raise ValidationError("Amount must be a positive real number")
+
+        cash_asset = user.assets.get(tr_eq=TradingEquipment.objects.get(sym='USD_USD'))
+
+        cash_asset.amount += amount
+        cash_asset.save()
+
+        return Response(AssetSerializer(cash_asset).data, status=status.HTTP_201_CREATED)
