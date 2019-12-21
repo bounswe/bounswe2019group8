f6db87b736ed6da8a71730c44b7fc39d147d0390
Alper Çakan <alpercakan98@gmail.com>
2019-12-21 16:48:28 +0300

    add endpoint for getting notification count

--- nova/nova/views/notifications.py
+++ nova/nova/views/notifications.py
@@ -28,3 +28,18 @@ def notifications_coll(request, user_pk):
             notification.save()
 
         return response
+
+
+@api_view(['GET'])
+@permission_classes((permissions.IsAuthenticated,))
+def notifications_coll_count(request, user_pk):
+    try:
+        user = User.objects.get(pk=user_pk)
+    except User.DoesNotExist:
+        raise NotFound()
+
+    if user_pk != request.user.pk:
+        raise PermissionDenied()
+
+    if request.method == 'GET':
+        return Response({"count": user.notifications.filter(is_read=False).count()}, status=status.HTTP_200_OK)
