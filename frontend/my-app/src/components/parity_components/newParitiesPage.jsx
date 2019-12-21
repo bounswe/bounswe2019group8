9b99f353e97cd11b172d5791a9d3c362e1ec1dbf
baris-zongur-2016400285 <bariszongur1@gmail.com>
2019-12-21 04:25:33 +0300

    Added buy/sell functionalities

--- frontend/my-app/src/components/parity_components/newParitiesPage.jsx
+++ frontend/my-app/src/components/parity_components/newParitiesPage.jsx
@@ -5,7 +5,8 @@ import {
     Button,
     Navbar,
     NavDropdown,
-    FormControl
+    FormControl,
+    Nav
   } from "react-bootstrap";
 import NavLinkComponent from "./navLinkComponents";
 import { FaSignOutAlt, FaListAlt, FaUserCircle, FaSearchDollar, FaThumbsDown } from "react-icons/fa";
@@ -15,35 +16,82 @@ class NewParitiesPage extends Component {
   state = {
   stockJson : JSON.parse(localStorage.getItem("equipmentList")),
   stockBadges:[],
+  assetsBadges:[],
+  forexBadges:[],
+  dollarBadge:[],
   firstBadge:[ <NavDropdown.Item>Not Selected</NavDropdown.Item>],
   secondBadge:[ <NavDropdown.Item>Not Selected</NavDropdown.Item>],
   firstParity:"Not Selected",
-  secondParity:"USD",
+  secondParity:"U.S. Dollar",
+  buyWithParity:"U.S. Dollar",
   firstSym:"",
-  secondSym:"",
-  firstType:"Not Selected",
-  SecondType:"Not Selected",
+  secondSym:"USD_USD",
+  buyWithSym:"USD_USD",
+  currentAmount:0,
+  currentBuyingAmount:0,
+  firstType:null,
+  SecondType:null,
+  buyType:null,
   parityPk:0,
+  buyWith:"",
   buyClicked:false,
   lastClicked:null,
   assets:null,
-  buyAmount:0
+  buyAmount:0,
+  refresh:true
    };
   componentDidMount(){
+    var count1 =0
+    var count2 =0;
     var allBadges ={commodity:[],stock:[],digital:[],forex:[],etf:[],index:[]};
     var stockJson = this.state.stockJson;
     var token = localStorage.getItem("userToken");
+    var forexBadges= [];
+    var dollarBadge=[];
     axios.get("http://8.209.81.242:8000/users/" + localStorage.getItem("userId")+"/assets", {
       headers: { Authorization: `Token ${token}` }
     }).then(res => {
       this.setState({assets: res.data});
+      for (var i = 0; i < res.data.length; i++) {
+          if(res.data[i].tr_eq.type === "forex"){
+            if(res.data[i].tr_eq.sym === "USD_USD"){
+              dollarBadge.push(
+                <NavLinkComponent thisName={res.data[i].tr_eq.name} thisSymbol = {res.data[i].tr_eq.sym} thisPk={res.data[i].tr_eq.pk} setParity={this.setParity}></NavLinkComponent>
+              )
+            }
+            forexBadges.push(
+              <NavLinkComponent thisName={res.data[i].tr_eq.name} thisSymbol = {res.data[i].tr_eq.sym} thisPk={res.data[i].tr_eq.pk} setParity={this.setParity}></NavLinkComponent>
+            )
+          }
+          if(res.data[i].tr_eq.sym === this.state.firstSym){
+            this.setState({currentBuyingAmount:res.data[i].amount})
+            count1 = count1 +1;
+          }
+          if(res.data[i].tr_eq.sym === this.state.buyWithSym){
+            this.setState({currentAmount:res.data[i].amount})
+            count2 = count2 +1;
+          } 
+      }
     });
+    if(count1===0){
+      this.setState({currentBuyingAmount:"0"})
+    }
+    if(count2===0){
+      this.setState({currentBuyingAmount:"0"})
+    }
     if(stockJson !== null){   
       for (var i = 0; i < stockJson.length; i++) {
         allBadges[stockJson[i].type].push(
-          <NavLinkComponent thisName={stockJson[i].name} thisSymbol = {stockJson[i].sym.split("_")[0]} thisPk={stockJson[i].pk} setParity={this.setParity}></NavLinkComponent>);
+          <NavLinkComponent thisName={stockJson[i].name} thisSymbol = {stockJson[i].sym} thisPk={stockJson[i].pk} setParity={this.setParity}></NavLinkComponent>);
       }
-      this.setState({stockBadges:allBadges})
+      
+    }
+    this.setState({forexBadges:forexBadges,stockBadges:allBadges, dollarBadge:dollarBadge})
+    if(this.state.firstType === "forex"){
+      this.setState({assetsBadges:forexBadges})
+    }
+    else{
+      this.setState({assetsBadges:dollarBadge})
     }
   }
   render() {
@@ -130,7 +178,49 @@ class NewParitiesPage extends Component {
           aria-controls="basic-navbar-nav"
         />
         <Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
-        <font color="white">Buy Amount</font> 
+        <Nav color = "white" >
+              <Nav.Link style={{color: 'white', textDecoration: 'none'}}>Buying</Nav.Link>
+        </Nav>
+        <FormControl
+                readOnly
+                type="text"
+                placeholder={this.state.firstParity}
+                className="mr-sm-2"
+              /> 
+         <Nav color = "white" >
+              <Nav.Link style={{color: 'white', textDecoration: 'none'}}>Current</Nav.Link>
+        </Nav>
+        <FormControl
+                readOnly
+                type="text"
+                placeholder={this.state.currentBuyingAmount}
+                className="mr-sm-2"
+              />      
+        <NavDropdown
+              title="Buy With"
+              id="basic-nav-dropdown"
+              onClick={() => this.setLastClicked("buywith")}
+            >
+          {this.state.assetsBadges}
+        </NavDropdown>
+        <FormControl
+                readOnly
+                type="text"
+                placeholder={this.state.buyWithParity}
+                className="mr-sm-2"
+        /> 
+        <Nav color = "white" >
+              <Nav.Link style={{color: 'white', textDecoration: 'none'}}>Current</Nav.Link>
+        </Nav>
+        <FormControl
+                readOnly
+                type="text"
+                placeholder={this.state.currentAmount}
+                className="mr-sm-2"
+              /> 
+          <Nav color = "white" >
+              <Nav.Link style={{color: 'white', textDecoration: 'none'}}>Buy Amount</Nav.Link>
+        </Nav>
         <FormControl
                 width = "%50"
                 input type="number"
@@ -138,7 +228,11 @@ class NewParitiesPage extends Component {
                 className="mr-sm-2"
                 onChange={this.changeBuy}
               
-              />
+        />
+         <Button onClick={()=> this.handleCheckoutClick()} id='searchButton'  variant="outline-success">
+                Checkout
+            <FaSearchDollar style={{marginLeft: 6}}></FaSearchDollar>
+            </Button>
         </Navbar>
         : <div/>
       }
@@ -153,12 +247,16 @@ class NewParitiesPage extends Component {
   }
   setParity = (name, pk, symbol) =>{
    if(this.state.lastClicked==="first"){
-    this.setState({firstParity:name, firstSym:symbol,parityPk:pk})
+    this.setState({firstParity:name, firstSym:symbol ,parityPk:pk})
     this.handleSearchClick()
+    window.setTimeout(this.handleCheckOut, 30)   
    }
    if(this.state.lastClicked==="second"){
     this.setState({secondParity:name, secondSym:symbol})
-  }
+   }
+   if(this.state.lastClicked==="buywith"){
+    this.setState({buyWithParity:name, buyWithSym:symbol})
+   }
 
   }
   setSecondParity = (firstParity2) =>{
@@ -190,6 +288,46 @@ class NewParitiesPage extends Component {
     this.setState({
       buyAmount: event.target.value
     });
+  }
+  handleCheckOut = () => {
+    var token = localStorage.getItem("userToken");
+    var count1=0
+    var count2=0
+    axios.get("http://8.209.81.242:8000/users/" + localStorage.getItem("userId")+"/assets", {
+      headers: { Authorization: `Token ${token}` }
+    }).then(res => {
+      this.setState({assets: res.data});
+      for (var i = 0; i < res.data.length; i++) {     
+          if(res.data[i].tr_eq.sym === this.state.firstSym){
+            this.setState({currentBuyingAmount:res.data[i].amount})
+            count1 = count1 +1;
+          }
+          if(res.data[i].tr_eq.sym === this.state.buyWithSym){
+            this.setState({currentAmount:res.data[i].amount})
+            count2 = count2 +1;
+          } 
+      }
+    });
+    if(count1===0){
+      this.setState({currentBuyingAmount:"0"})
+    }
+    if(count2===0){
+      this.setState({currentBuyingAmount:"0"})
+    }   
+  }
+  handleCheckoutClick =()=>{
+    axios.post("http://8.209.81.242:8000/users/" + localStorage.getItem("userId")+"/assets", 
+    {
+      sell_amount: 10,
+      buy_tr_eq_sym:this.state.firstySym,
+      sell_tr_eq_sym:this.state.buyWithSym
+    },
+    {
+      headers: {
+        Authorization: `Token ${localStorage.getItem("userToken")}`
+      }
+    })
+    window.setTimeout(this.handleCheckOut, 30)   
   }
 }
 
