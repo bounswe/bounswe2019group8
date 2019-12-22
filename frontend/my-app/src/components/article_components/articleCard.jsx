773d33fa1776fb17198337defa122abd8a1e7204
Alper Çakan <alpercakan98@gmail.com>
2019-12-22 21:55:03 +0300

    use text truncate

--- frontend/my-app/src/components/article_components/articleCard.jsx
+++ frontend/my-app/src/components/article_components/articleCard.jsx
@@ -21,7 +21,7 @@ class ArticleCard extends Component {
           <Button style={{fontWeight:'bolder', fontSize:11, height:30, width:'auto', marginBottom:16}} id='writtenBy' href={"/profile/" + this.props.articleAuthorId} variant="primary">
             <FaFeatherAlt style={{marginRight:4}}></FaFeatherAlt>by {this.state.articleAuthorId}
           </Button>
-          <Card.Text id='cardContent' style={{  padding: 10 }}>{this.state.articleContent}</Card.Text>
+          <Card.Text id='cardContent' style={{  padding: 10 }} className="text-truncate">{this.props.articleContent}</Card.Text>
 
           <Button id='articleCardButton' href={"/articles/" + this.props.articlePk} block variant="outline-primary">
             <FaRegNewspaper style={{marginRight:4}}></FaRegNewspaper>Read the article
@@ -31,20 +31,6 @@ class ArticleCard extends Component {
     );
   }
   componentDidMount() {
-    var myContent =
-      this.props.articleContent[0] +
-      this.props.articleContent[1] +
-      this.props.articleContent[2] +
-      this.props.articleContent[3] +
-      this.props.articleContent[4] +
-      this.props.articleContent[5] +
-      this.props.articleContent[6] +
-      this.props.articleContent[7] +
-      this.props.articleContent[8] +
-      this.props.articleContent[9] +
-      this.props.articleContent[10] +
-      this.props.articleContent[11];
-    this.setState({ articleContent: myContent });
     var token = localStorage.getItem("userToken");
     axios
       .get("http://8.209.81.242:8000/users/" + this.props.articleAuthorId, {
